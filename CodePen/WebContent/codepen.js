fc86a6db025a19f6302d9321f7efab5cdfbe2043
aa69wa <aa69wa@gmail.com.au>
2014-11-17 21:02:55 +0800

    Initial checkin of ResponsiveTest

---
+++ CodePen/WebContent/codepen.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	
+	var tp = $('.top p');
+	var ct = $('.container');
+	var lt = $('.left');
+	var mt = $('.middle');
+	var sb = $('.sidebar');
+	var sideNav = $(".nav-sidebar");
+
+	function doResize(e) {
+		var ww = $(window).width();
+	 
+		tp.text("Container: " + ct.width() + " - Window: " + ww);
+		$('.left p').text("Step: " + lt.width());
+		$('.middle p').text("Content: "+ mt.width());
+		$('.right p').text("Side Bar: "+ sb.width());
+	}
+
+	// Handle change of media query & initial load ...
+	var handleMediaChange = function (mediaQueryList) {
+		var initial = document.readyState === "complete" ? false : true;
+		
+	    if (mediaQueryList.matches) {
+	    	if (initial) {					// The browser window is 1200px or less
+    		sb.addClass( 'hide');			// Set side bar initial state
+	    	} else {
+	    		hideSide();					
+	    	}
+	    } else {
+	    	if (!initial) {
+	    		showSide();					// The browser window is 1200px or less
+	    	}
+	    }
+	};		
+	
+	function hideSide() {
+		var sb = $('.sidebar');
+		sb.fadeOut(500, function() { sb.addClass('hide'); });
+	} 
+
+	function showSide() {
+		var sb = $('.sidebar');
+		sb.fadeIn(500, function() { sb.removeClass('hide'); }); 
+	} 
+
+	$(window).on('resize', doResize);		// Check area sizes (temporary - testing only)
+
+	// Toggle the side bar on click of the side bar button ...
+	sideNav.on('click', function(event) { sb.hasClass('hide') ? showSide(): hideSide(); });
+
+	// Listener for change of media query ..
+	var mql = window.matchMedia("(max-width: 1200px)");
+	mql.addListener(handleMediaChange);
+	handleMediaChange(mql);
+});
---
+++ CodePen/WebContent/codepen.jsp
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CodePen HR Forms Mock</title>
+        <link type="text/css" rel="stylesheet" href="codepen.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />   
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.js"></script>
+		<script type="text/javascript" src="polyfills/matchMedia.js"></script>
+        <script type="text/javascript" src="polyfills/matchMedia.addListener.js"></script>		      
+        <script type="text/javascript" src="codepen.js"></script>
+    </head>
+
+    <body>
+		<div class=top><p></p></div>
+		<div class="container">
+		  <header>
+		  	<h1>CodePen HR Forms Mock</h1>
+		  	<nav><button class="nav-sidebar">Sidebar</button></nav>
+		    <div style="clear: both"></div>
+		  </header> 
+		
+		  <main> 
+		    <section class="left">
+				<p></p>
+		        <button class="nav-back">Back</button>
+		    </section>
+		    <section class="middle">
+		      <p></p>
+		      <button class="nav-next">Next</button>
+		    </section> 
+		  </main>  
+		
+		  <aside class="sidebar">
+		    <div class="right"><p></p></div>
+		  </aside>
+		</div>
+    </body>
+</html>
